b33694b1ea3d443d10e8be7fb47b1daeade99b3e
Romaric250 <lonfonyuyromeo@gmail.com>
2024-02-29 15:01:29 +0100

    updated some other cool stuff

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -23,55 +23,25 @@ const assert = require('assert');
 const calculateNumber = require('./1-calcul');
 
 describe('with type SUM', () => {
-  it('add 5 and 10', () => {
-    assert.equal(calculateNumber('SUM', 5, 10), 15);
-  });
-  it('add 2 and 2.7', () => {
-    assert.equal(calculateNumber('SUM', 2, 2.7), 5);
-  });
-  it('add 1.0 and 4.0', () => {
-    assert.equal(calculateNumber('SUM', 1.0, 4.0), 5);
-  });
+  
   it('add 1.7 and 3.5', () => {
     assert.equal(calculateNumber('SUM', 1.7, 3.5), 6);
   });
 });
 
 describe('with type SUBTRACT', () => {
-  it('subtract 1 and 3', () => {
-    assert.equal(calculateNumber('SUBTRACT', 1, 3), -2);
-  });
+  
   it('subtract 1.4 and 4.5', () => {
     assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
-  });
-  it('subtract 1.2 and 3.7', () => {
-    assert.equal(calculateNumber('SUBTRACT', 1.2, 3.7), -3);
-  });
-  it('subtract 1.5 and 3.7', () => {
-    assert.equal(calculateNumber('SUBTRACT', 1.5, 3.7), -2);
-  });
-  it('subtract 0.1 and 0.3', () => {
-    assert.equal(calculateNumber('SUBTRACT', 0.1, 0.3), 0);
   });
   
 });
 
 describe(' with type DIVIDE', () => {
-  it('divide 1 and 4', () => {
-    assert.equal(calculateNumber('DIVIDE', 1, 4), 0.25);
-  });
-  it('divide 1 and 3.7', () => {
-    assert.equal(calculateNumber('DIVIDE', 1, 3.7), 0.25);
-  });
   it('divide 1.4 and 4.5', () => {
     assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
   });
-  it('divide 1.6 and 2.4', () => {
-    assert.equal(calculateNumber('DIVIDE', 1.6, 2.4), 1);
-  });
-  it('divide 4.2 and 1.5', () => {
-    assert.equal(calculateNumber('DIVIDE', 4.2, 1.5), 2);
-  });
+  
   it("divide 1.3 and 0.3", () => {
     assert.equal(calculateNumber('DIVIDE', 1.3, 0.3), 'Error');
   });
