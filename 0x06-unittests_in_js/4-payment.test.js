e86570c743cc8ad761f915164faad67e6022f26c
Romaric250 <lonfonyuyromeo@gmail.com>
2024-02-29 18:26:10 +0100

    update some stuffs more is comming on the way

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,27 @@
+const sendPaymentRequestToApi = require('./4-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  let stubbing;
+
+  beforeEach(() => {
+    stubbing = sinon.stub(Utils, 'calculateNumber').returns(10);
+  });
+
+  afterEach(() => {
+    stubbing.restore();
+  });
+
+  it('sendPaymentRequestToApi with utils', () => {
+    const spy = sinon.spy(console, 'log');
+
+    sendPaymentRequestToApi(100, 20);
+
+    sinon.assert.calledOnce(stubbing);
+    sinon.assert.calledWith(stubbing, 'SUM', 100, 20);
+
+    sinon.assert.calledOnce(spy);
+    sinon.assert.calledWith(spy, 'The total is: 10');
+
+    spy.restore();
+  });
+});
