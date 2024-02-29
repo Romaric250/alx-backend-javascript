c684f2b0ff057be6f7ba9f1aeacb500fe3402571
Romaric250 <lonfonyuyromeo@gmail.com>
2024-02-29 14:36:15 +0100

    updated some things;

---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,19 @@
+const calculateNumber = (type,a, b) => { 
+    const num1 = Math.round(a);
+    const num2 = Math.round(b);
+    
+    if (type === 'SUM') {
+        return num1 + num2;
+    }
+    if (type === 'SUBTRACT') {
+      return num1 - num2;
+    }
+    if (type === 'DIVIDE') {
+      if (rounded_b === 0) {
+        return 'Error';
+      }
+      return num1 / num2;
+    }
+  }
+  
+  module.exports = calculateNumber;
